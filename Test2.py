04e9160d5153705b4c88b5ad509ea5778310512f
Muichiro01 <naureenaayat@gmail.com>
2026-07-25 21:15:58 +0530

    Add files via upload

---
+++ Test2.py
@@ -0,0 +1,64 @@
+"""Build a calculator that uses a separate function for each operation. The user picks an operation and enters two numbers. Your program handles invalid input and division by zero without crashing.
+
+1.  def and return     →  define 4 functions: add, subtract, multiply, divide
+2.  try/except         →  catch ZeroDivisionError and ValueError without crashing
+3.  float(input())     →  to read numbers from the user
+4.  return values      →  each function must return the correct result"""
+def(add):
+try:
+    num1=float(input("Enter 1st number: "))
+    except ValueError
+    num1=0
+    except ZeroDivisionError
+    num1=0
+    num2=float(input("Enter 2nd number"))
+    except ValueError
+    num2=0
+    except ZeroDivisionError
+    num2=0
+print(a+b)
+
+def(subtract):
+try:
+    num1=float(input("Enter 1st number: "))
+    except ValueError
+    num1=0
+    except ZeroDivisionError
+    num1=0
+    num2=float(input("Enter 2nd number"))
+    except ValueError
+    num2=0
+    except ZeroDivisionError
+    num2=0
+print(a-b)
+
+def(multiply):
+try:
+    num1=float(input("Enter 1st number: "))
+    except ValueError
+    num1=0
+    except ZeroDivisionError
+    num1=0
+    num2=float(input("Enter 2nd number"))
+    except ValueError
+    num2=0
+    except ZeroDivisionError
+    num2=0
+print(a*b)
+
+def(divide):
+try:
+    num1=float(input("Enter 1st number: "))
+    except ValueError
+    num1=0
+    except ZeroDivisionError
+    num1=0
+    num2=float(input("Enter 2nd number"))
+    except ValueError
+    num2=0
+    except ZeroDivisionError
+    num2=0
+print(a/b)
+
+option=input("Enter the operation:add,subtract,multiply or divide: ")
+if option=="add"
